22636b2cfcea4bdf1579b3e54300fa863870c342
Lucien <ls.hyjing@gmail.com>
2025-05-31 10:55:26 -0400

    Remove outdated comments in main.rs for clarity

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,9 @@ use std::io::Result;
 mod ast;
 mod ir;
 
-// 引用 lalrpop 生成的解析器
 lalrpop_mod!(sysy);
 
 fn main() -> Result<()> {
-    // 解析命令行参数
     let mut args = args();
     args.next();
     let mode = args.next().unwrap();
@@ -17,13 +15,10 @@ fn main() -> Result<()> {
     args.next();
     let outfile = args.next().unwrap();
 
-    // 读取输入文件
     let input = read_to_string(input)?;
 
-    // 调用 lalrpop 生成的 parser 解析输入文件
     let ast = sysy::CompUnitParser::new().parse(&input).unwrap();
 
-    // 输出解析得到的 AST
     // println!("{:#?}", ast);
     let koopa_ir = ir::ast2ir(&ast);
     if mode == "-koopa" {
