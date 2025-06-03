d1ef368596234eb480c9648db44c8fc456c2b566
Lucien <ls.hyjing@gmail.com>
2025-06-03 11:38:26 -0400

    Refactor assembly code in hello.asm; enhance logic for conditional checks and arithmetic operations

--- hello.asm
+++ hello.asm
@@ -1,18 +1,30 @@
 .text
 .globl main
 main:
-li t0, 1
-li t1, 2
-slt t2, t1, t0
-xori t2, t2, 1
-li t3, 1
-sub t4, x0, t3
-li t5, 3
-add t6, t5, t4
-li t7, 4
-slt t8, t7, t6
-slt t9, x0, t2
-slt t10, x0, t8
-and t11, t9, t10
-mv a0, t11
+li t0, 3
+xor t1, x0, t0
+seqz t1, t1
+xor t2, x0, t1
+snez t2, t2
+li t3, 4
+xor t4, x0, t3
+snez t4, t4
+or t5, t2, t4
+li t6, 2
+mul t7, t6, t5
+li t8, 1
+add t9, t8, t7
+li t10, 5
+slt t11, t9, t10
+li t12, 6
+xor t13, t11, t12
+snez t13, t13
+li t14, 7
+sub t15, x0, t14
+xor t16, x0, t13
+snez t16, t16
+xor t17, x0, t15
+snez t17, t17
+and t18, t16, t17
+mv a0, t18
 ret
