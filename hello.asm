7cb8403280cc47108e04614ee7131f7c7352ca87
Lucien <ls.hyjing@gmail.com>
2025-06-05 16:16:40 -0400

    Refactor hello.asm to implement a new main function logic; update ir2riscv.rs for improved stack management and function handling; comment out debug print in main.rs for cleaner output.

--- hello.asm
+++ hello.asm
@@ -1,8 +1,19 @@
 .text
 .globl main
 main:
-li t0, 2
-li t1, 6
-add t2, t0, t1
-mv a0, t2
+addi sp, sp, -48
+li t0, 10
+sw t0, 0(sp)
+lw t0, 0(sp)
+sw t0, 4(sp)
+lw t0, 4(sp)
+li t1, 1
+add t0, t0, t1
+sw t0, 8(sp)
+lw t0, 8(sp)
+sw t0, 0(sp)
+lw t0, 0(sp)
+sw t0, 12(sp)
+lw a0, 12(sp)
+addi sp, sp, 48
 ret
--- src/ir2riscv.rs
+++ src/ir2riscv.rs
@@ -5,16 +5,27 @@ pub fn ir2riscv(ir: String) -> String {
     let driver = koopa::front::Driver::from(ir);
     let program = driver.generate_program().unwrap();
     out += ".text\n";
-
     for &func in program.func_layout() {
         let func_data = program.func(func);
         out += &format!(".globl {}\n", &func_data.name()[1..]);
         out += &format!("{}:\n", &func_data.name()[1..]);
-        let mut reg_map: HashMap<koopa::ir::Value, String> = HashMap::new();
-        let mut reg_count = 0;
+        let mut stack_map: HashMap<koopa::ir::Value, String> = HashMap::new();
+        let mut stack_offset = -4;
+        let mut size = func_data.dfg().values().len() * 4;
+        if size % 16 != 0 {
+            size += 16 - size % 16;
+        }
+        out += &format!("addi sp, sp, -{}\n", size);
         for (&_, node) in func_data.layout().bbs() {
             for &inst in node.insts().keys() {
-                let code = stmt2str(func_data, &inst, &mut reg_map, &mut reg_count);
+                let code = stmt2str(
+                    func_data,
+                    &inst,
+                    0,
+                    &mut stack_map,
+                    &mut stack_offset,
+                    size as i32,
+                );
                 out += &code;
             }
         }
@@ -24,44 +35,64 @@ pub fn ir2riscv(ir: String) -> String {
 fn stmt2str(
     func_data: &koopa::ir::FunctionData,
     value: &koopa::ir::Value,
-    reg_map: &mut HashMap<koopa::ir::Value, String>,
-    reg_count: &mut usize,
+    reg_count: usize,
+    stack_map: &mut HashMap<koopa::ir::Value, String>,
+    stack_offset: &mut i32,
+    size: i32,
 ) -> String {
-    // println!("{:?}", func_data.dfg().value(*value).kind());
+    let mut out = String::new();
     match func_data.dfg().value(*value).kind() {
-        koopa::ir::ValueKind::Integer(int) => {
-            if int.value() == 0 {
-                reg_map.insert(*value, "x0".to_string());
-                return String::new();
-            }
-            let reg = *reg_count;
-            *reg_count += 1;
-            reg_map.insert(*value, format!("t{}", reg));
-            format!("li t{}, {}\n", reg, int.value())
-        }
+        koopa::ir::ValueKind::Integer(int) => out = format!("li t{}, {}\n", reg_count, int.value()),
         koopa::ir::ValueKind::Return(ret) => {
-            let mut out = String::new();
-            if !reg_map.contains_key(&ret.value().unwrap()) {
-                out += &stmt2str(func_data, &ret.value().unwrap(), reg_map, reg_count);
+            if ret.value().is_none() {
+                out += &format!("addi sp, sp, {}\n", size);
+                out += &format!("ret\n");
+            } else {
+                if stack_map.contains_key(&ret.value().unwrap()) {
+                    out += &format!("lw a0, {}\n", stack_map.get(&ret.value().unwrap()).unwrap());
+                } else {
+                    out += &stmt2str(
+                        func_data,
+                        &ret.value().unwrap(),
+                        reg_count,
+                        stack_map,
+                        stack_offset,
+                        size,
+                    );
+                    out += &format!("mv a0, t{}\n", reg_count);
+                }
+                out += &format!("addi sp, sp, {}\n", size);
+                out += &format!("ret\n");
             }
-            let val_reg = reg_map.get(&ret.value().unwrap()).unwrap();
-            out += &format!("mv a0, {}\nret\n", val_reg);
-            out
         }
         koopa::ir::ValueKind::Binary(bin) => {
-            let mut out = String::new();
-            if !reg_map.contains_key(&bin.lhs()) {
-                out += &stmt2str(func_data, &bin.lhs(), reg_map, reg_count);
+            let dest_reg = format!("t{}", reg_count);
+            let lhs_reg = format!("t{}", reg_count);
+            let rhs_reg = format!("t{}", reg_count + 1);
+            if stack_map.contains_key(&bin.lhs()) {
+                out += &format!("lw {}, {}\n", lhs_reg, stack_map.get(&bin.lhs()).unwrap());
+            } else {
+                out += &stmt2str(
+                    func_data,
+                    &bin.lhs(),
+                    reg_count,
+                    stack_map,
+                    stack_offset,
+                    size,
+                );
             }
-            if !reg_map.contains_key(&bin.rhs()) {
-                out += &stmt2str(func_data, &bin.rhs(), reg_map, reg_count);
+            if stack_map.contains_key(&bin.rhs()) {
+                out += &format!("lw {}, {}\n", rhs_reg, stack_map.get(&bin.rhs()).unwrap());
+            } else {
+                out += &stmt2str(
+                    func_data,
+                    &bin.rhs(),
+                    reg_count + 1,
+                    stack_map,
+                    stack_offset,
+                    size,
+                );
             }
-            let dest_reg = *reg_count;
-            *reg_count += 1;
-            reg_map.insert(*value, format!("t{}", dest_reg));
-            let dest_reg = reg_map.get(value).unwrap();
-            let lhs_reg = reg_map.get(&bin.lhs()).unwrap();
-            let rhs_reg = reg_map.get(&bin.rhs()).unwrap();
             match bin.op() {
                 koopa::ir::BinaryOp::Add => {
                     out += &format!("add {}, {}, {}\n", dest_reg, lhs_reg, rhs_reg);
@@ -103,10 +134,49 @@ fn stmt2str(
                     out += &format!("slt {}, {}, {}\n", dest_reg, rhs_reg, lhs_reg);
                     out += &format!("xori {}, {}, 1\n", dest_reg, dest_reg);
                 }
-                _ => unreachable!()
+                _ => unreachable!(),
             }
-            out
+            out += &format!("sw {}, {}(sp)\n", dest_reg, *stack_offset + 4);
+            *stack_offset += 4;
+            stack_map.insert(*value, format!("{}(sp)", stack_offset));
+        }
+        koopa::ir::ValueKind::Alloc(_) => {}
+        koopa::ir::ValueKind::Store(store) => {
+            if stack_map.contains_key(&store.value()) {
+                out += &format!(
+                    "lw t{}, {}\n",
+                    reg_count,
+                    stack_map.get(&store.value()).unwrap()
+                );
+            } else {
+                out += &stmt2str(
+                    func_data,
+                    &store.value(),
+                    reg_count,
+                    stack_map,
+                    stack_offset,
+                    size,
+                );
+            }
+            if !stack_map.contains_key(&store.dest()) {
+                *stack_offset += 4;
+                stack_map.insert(store.dest(), format!("{}(sp)", stack_offset));
+            }
+            out += &format!("sw t{}, {}\n", reg_count, stack_map.get(&store.dest()).unwrap());
+        }
+        koopa::ir::ValueKind::Load(load) => {
+            out += &format!(
+                "lw t{}, {}\n",
+                reg_count,
+                stack_map.get(&load.src()).unwrap()
+            );
+            *stack_offset += 4;
+            stack_map.insert(*value, format!("{}(sp)", stack_offset));
+            out += &format!("sw t{}, {}(sp)\n", reg_count, stack_offset);
         }
         _ => unreachable!(),
     }
+    println!("{:?}", func_data.dfg().value(*value).kind());
+    println!("out:\n{}", out);
+    out
 }
