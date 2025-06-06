693241210c7d637b224f4c3579c0149478050b13
Lucien <ls.hyjing@gmail.com>
2025-06-06 14:31:43 -0400

    Refactor hello.asm to simplify main function logic; update stack operations and variable handling for improved clarity and efficiency.

--- hello.asm
+++ hello.asm
@@ -2,18 +2,14 @@
 .globl main
 main:
 addi sp, sp, -48
-li t0, 10
+li t0, 1
 sw t0, 0(sp)
-lw t0, 0(sp)
+li t0, 2
+sw t0, 0(sp)
+li t0, 3
 sw t0, 4(sp)
-lw t0, 4(sp)
-li t1, 1
-add t0, t0, t1
-sw t0, 8(sp)
-lw t0, 8(sp)
-sw t0, 0(sp)
 lw t0, 0(sp)
-sw t0, 12(sp)
-lw a0, 12(sp)
+sw t0, 8(sp)
+lw a0, 8(sp)
 addi sp, sp, 48
 ret
