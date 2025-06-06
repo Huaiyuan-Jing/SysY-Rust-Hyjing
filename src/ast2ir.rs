c743bcc63d7ddb51e4f1297d2d0a42481c36eecf
Lucien <ls.hyjing@gmail.com>
2025-06-06 14:28:27 -0400

    Refactor hello.c and hello.koopa to update main function logic; modify ast2ir.rs for improved variable handling and expression evaluation; adjust sysy.lalrpop grammar for optional expressions.

--- src/ast2ir.rs
+++ src/ast2ir.rs
@@ -1,34 +1,39 @@
 use crate::ast::{self, BlockItem, FuncType, Stmt};
 use lazy_static::lazy_static;
-use std::{collections::HashMap, sync::Mutex};
+use std::{collections::HashMap, fmt::format, sync::Mutex};
 lazy_static! {
     static ref COUNTER: Mutex<i32> = Mutex::new(-1);
+    static ref BLOCK_COUNTER: Mutex<i32> = Mutex::new(-1);
 }
 enum IdElement {
     Const(i32),
     Var(String),
 }
-struct IdTable {
-    table:HashMap<String, IdElement>,
-    father: Option<Box<IpTable>>, 
+struct IdTable<'a> {
+    table: HashMap<String, IdElement>,
+    father: Option<&'a IdTable<'a>>,
+    offset: i32,
 }
-impl IdTable {
-    pub fn new(father: Option<Box<IpTable>>) -> Self {
+impl<'a> IdTable<'a> {
+    pub fn new(father: Option<&'a IdTable<'a>>, offset: i32) -> Self {
         IdTable {
             table: HashMap::new(),
             father: father,
+            offset: offset,
         }
     }
-    pub fn insert(k: String, v: IdElement) -> bool {
-        self.table.insert(k, v).is_none()
+    pub fn insert(&mut self, k: String, v: IdElement) -> bool {
+        self.table
+            .insert(format!("{}_{}", k, self.offset), v)
+            .is_none()
     }
-    pub fn get(k: &String) -> Option<&IdElement> {
-        if self.table.contains_key(k) {
-            Some(&self.table[k])
+    pub fn get(&self, k: &String) -> (Option<&IdElement>, String) {
+        if self.table.contains_key(&format!("{}_{}", k, self.offset)) {
+            (Some(&self.table[&format!("{}_{}", k, self.offset)]), format!("{}_{}", k, self.offset))
         } else if self.father.is_some() {
             self.father.as_ref().unwrap().get(k)
         } else {
-            None
+            (None, String::new())
         }
     }
 }
@@ -40,34 +45,42 @@ pub fn ast2ir(ast: &ast::CompUnit) -> String {
         "fun @{}(): {} {{\n%entry:\n",
         &ast.func_def.ident, func_type
     );
-    out += &block2ir(&ast.func_def.block, &IdTable::new(None));
+    let id = {
+        let mut counter_guard = BLOCK_COUNTER.lock().unwrap();
+        *counter_guard += 1;
+        counter_guard.clone()
+    };
+    let mut table = IdTable::new(None, id);
+    out += &block2ir(&ast.func_def.block, &mut table);
     out += "}\n";
     out
 }
-fn block2ir(block: &ast::Block, id_table: &IdTable) -> String {
+fn block2ir(block: &ast::Block, id_table: &mut IdTable) -> String {
+    println!("Block: {}\n", id_table.offset);
     let mut out = String::new();
     for item in &block.items {
+        println!("{:#?}", item);
         match item {
             BlockItem::ConstDecl(clist) => {
                 for c in clist {
                     let id = c.id.clone();
-                    let val = compute_expr(&c.value);
+                    let val = compute_expr(&c.value, id_table);
                     id_table.insert(id, IdElement::Const(val));
                 }
             }
             BlockItem::VarDecl(vlsit) => {
                 for v in vlsit {
                     let id = v.id.clone();
-                    out += &format!("@{} = alloc {}\n", id, "i32");
+                    out += &format!("@{}_{} = alloc {}\n", id, id_table.offset, "i32");
                     if !v.value.is_none() {
-                        let tmp = expr2ir(&(v.value.as_ref()).unwrap());
+                        let tmp = expr2ir(&(v.value.as_ref()).unwrap(), id_table);
                         let pos = if tmp.0 == String::new() {
                             tmp.1.to_string()
                         } else {
                             format!("%{}", tmp.1)
                         };
                         out += &tmp.0;
-                        out += &format!("store {}, @{}\n", pos, id);
+                        out += &format!("store {}, @{}_{}\n", pos, id, id_table.offset);
                     }
                     id_table.insert(id, IdElement::Var(String::from("i32")));
                 }
@@ -75,7 +88,7 @@ fn block2ir(block: &ast::Block, id_table: &IdTable) -> String {
             BlockItem::Stmt(s) => match s {
                 Stmt::Ret(e) => match e {
                     Some(e) => {
-                        let tmp = expr2ir(e);
+                        let tmp = expr2ir(e, id_table);
                         let pos = if tmp.0 == String::new() {
                             tmp.1.to_string()
                         } else {
@@ -89,7 +102,16 @@ fn block2ir(block: &ast::Block, id_table: &IdTable) -> String {
                     }
                 },
                 Stmt::Assign(id, e) => {
-                    let tmp = expr2ir(e);
+                    let id = id_table.get(id);
+                    if id.0.is_none() {
+                        panic!("Unable to Find Variable");
+                    }
+                    match id.0.unwrap() {
+                        IdElement::Var(_) => {},
+                        _ => panic!("assign to non-variable"),
+                    }
+                    let id = id.1;
+                    let tmp = expr2ir(e, id_table);
                     let pos = if tmp.0 == String::new() {
                         tmp.1.to_string()
                     } else {
@@ -97,28 +119,34 @@ fn block2ir(block: &ast::Block, id_table: &IdTable) -> String {
                     };
                     out += &tmp.0;
                     out += &format!("store {}, @{}\n", pos, id);
-                },
+                }
                 Stmt::Expr(e) => match e {
                     Some(e) => {
-                        out += &expr2ir(exp).0;
+                        out += &expr2ir(e, id_table).0;
                     }
                     None => {}
-                }
+                },
                 Stmt::Block(b) => {
-                    out += &block2ir(b, &IdTable::new(Some(id_table)));
-                } 
-                _ => todo!()
+                    let id = {
+                        let mut counter_guard = BLOCK_COUNTER.lock().unwrap();
+                        *counter_guard += 1;
+                        counter_guard.clone()
+                    };
+                    let mut table = IdTable::new(Some(id_table), id);
+                    out += &block2ir(b, &mut table);
+                }
+                _ => todo!(),
             },
             _ => unreachable!(),
         }
     }
     out
 }
-fn compute_expr(expr: &ast::Expr) -> i32 {
+fn compute_expr(expr: &ast::Expr, id_table: &IdTable) -> i32 {
     match expr {
         ast::Expr::Number(n) => *n,
         ast::Expr::UnaryExpr(op, expr) => {
-            let out = compute_expr(expr.as_ref());
+            let out = compute_expr(expr.as_ref(), id_table);
             match op {
                 ast::UnaryOp::Not => {
                     if out == 0 {
@@ -132,8 +160,8 @@ fn compute_expr(expr: &ast::Expr) -> i32 {
             }
         }
         ast::Expr::BinaryExpr(lhs, op, rhs) => {
-            let lhs_val = compute_expr(lhs.as_ref());
-            let rhs_val = compute_expr(rhs.as_ref());
+            let lhs_val = compute_expr(lhs.as_ref(), id_table);
+            let rhs_val = compute_expr(rhs.as_ref(), id_table);
             match op {
                 ast::BinaryOp::Plus => lhs_val + rhs_val,
                 ast::BinaryOp::Minus => lhs_val - rhs_val,
@@ -199,50 +227,47 @@ fn compute_expr(expr: &ast::Expr) -> i32 {
             }
         }
         ast::Expr::LVal(lval) => {
-            match id_table.get(lval).unwrap() {
-                IdElement::Const(v) => *v,
-                _ => unreachable!(),
+            let out = id_table.get(lval);
+            if out.0.is_none() {
+                panic!("Unable to Find Value")
+            }
+            match out.0.unwrap() {
+                IdElement::Var(_) => panic!("Unable to calculate"),
+                IdElement::Const(c) => *c,
             }
         }
     }
 }
-fn expr2ir(exp: &ast::Expr) -> (String, i32) {
+fn expr2ir(exp: &ast::Expr, id_table: &IdTable) -> (String, i32) {
+    // println!("{:#?}", exp);
     match exp {
         ast::Expr::Number(n) => (String::new(), *n),
         ast::Expr::UnaryExpr(op, expr) => {
-            let out = expr2ir(expr);
+            let out = expr2ir(expr, id_table);
             let mut counter = COUNTER.lock().unwrap();
+            *counter += 1;
+            let counter = counter.clone();
             let pos = if out.0 == "".to_string() {
                 out.1.to_string()
             } else {
                 format!("%{}", out.1)
             };
             match op {
-                ast::UnaryOp::Not => {
-                    *counter += 1;
-                    (
-                        format!("{}%{} = eq 0, {}\n", out.0, *counter, pos),
-                        *counter,
-                    )
-                }
+                ast::UnaryOp::Not => (format!("{}%{} = eq 0, {}\n", out.0, counter, pos), counter),
                 ast::UnaryOp::Minus => {
-                    *counter += 1;
-                    (
-                        format!("{}%{} = sub 0, {}\n", out.0, *counter, pos),
-                        *counter,
-                    )
+                    (format!("{}%{} = sub 0, {}\n", out.0, counter, pos), counter)
                 }
                 ast::UnaryOp::Plus => (out.0, out.1),
             }
         }
         ast::Expr::BinaryExpr(lhs, op, rhs) => {
-            let lout = expr2ir(lhs);
+            let lout = expr2ir(lhs, id_table);
             let lpos = if lout.0 == String::new() {
                 lout.1.to_string()
             } else {
                 format!("%{}", lout.1)
             };
-            let rout = expr2ir(rhs);
+            let rout = expr2ir(rhs, id_table);
             let rpos = if rout.0 == String::new() {
                 rout.1.to_string()
             } else {
@@ -251,81 +276,87 @@ fn expr2ir(exp: &ast::Expr) -> (String, i32) {
             let mut out = format!("{}{}", lout.0, rout.0);
             let mut counter = COUNTER.lock().unwrap();
             *counter += 1;
+            let counter = counter.clone();
             match op {
                 ast::BinaryOp::Plus => {
                     out += &format!("%{} = add {}, {}\n", counter, lpos, rpos);
-                    (out, *counter)
+                    (out, counter)
                 }
                 ast::BinaryOp::Minus => {
                     out += &format!("%{} = sub {}, {}\n", counter, lpos, rpos);
-                    (out, *counter)
+                    (out, counter)
                 }
                 ast::BinaryOp::Multiply => {
                     out += &format!("%{} = mul {}, {}\n", counter, lpos, rpos);
-                    (out, *counter)
+                    (out, counter)
                 }
                 ast::BinaryOp::Divide => {
                     out += &format!("%{} = div {}, {}\n", counter, lpos, rpos);
-                    (out, *counter)
+                    (out, counter)
                 }
                 ast::BinaryOp::Modulo => {
                     out += &format!("%{} = mod {}, {}\n", counter, lpos, rpos);
-                    (out, *counter)
+                    (out, counter)
                 }
                 ast::BinaryOp::Eq => {
                     out += &format!("%{} = eq {}, {}\n", counter, lpos, rpos);
-                    (out, *counter)
+                    (out, counter)
                 }
                 ast::BinaryOp::Neq => {
                     out += &format!("%{} = ne {}, {}\n", counter, lpos, rpos);
-                    (out, *counter)
+                    (out, counter)
                 }
                 ast::BinaryOp::Less => {
                     out += &format!("%{} = lt {}, {}\n", counter, lpos, rpos);
-                    (out, *counter)
+                    (out, counter)
                 }
                 ast::BinaryOp::LessOrEqual => {
                     out += &format!("%{} = le {}, {}\n", counter, lpos, rpos);
-                    (out, *counter)
+                    (out, counter)
                 }
                 ast::BinaryOp::Greater => {
                     out += &format!("%{} = lt {}, {}\n", counter, rpos, lpos);
-                    (out, *counter)
+                    (out, counter)
                 }
                 ast::BinaryOp::GreaterOrEqual => {
                     out += &format!("%{} = le {}, {}\n", counter, rpos, lpos);
-                    (out, *counter)
+                    (out, counter)
                 }
                 ast::BinaryOp::And => {
                     out += &format!("%{} = ne {}, {}\n", counter, 0, lpos);
-                    let lpos = format!("%{}", *counter);
+                    let mut counter = COUNTER.lock().unwrap();
+                    let lpos = format!("%{}", counter);
                     *counter += 1;
                     out += &format!("%{} = ne {}, {}\n", counter, 0, rpos);
-                    let rpos = format!("%{}", *counter);
+                    let rpos = format!("%{}", counter);
                     *counter += 1;
                     out += &format!("%{} = and {}, {}\n", counter, lpos, rpos);
-                    (out, *counter)
+                    (out, counter.clone())
                 }
                 ast::BinaryOp::Or => {
                     out += &format!("%{} = ne {}, {}\n", counter, 0, lpos);
-                    let lpos = format!("%{}", *counter);
+                    let mut counter = COUNTER.lock().unwrap();
+                    let lpos = format!("%{}", counter);
                     *counter += 1;
                     out += &format!("%{} = ne {}, {}\n", counter, 0, rpos);
-                    let rpos = format!("%{}", *counter);
+                    let rpos = format!("%{}", counter);
                     *counter += 1;
                     out += &format!("%{} = or {}, {}\n", counter, lpos, rpos);
-                    (out, *counter)
+                    (out, counter.clone())
                 } // _ => unreachable!(),
             }
         }
         ast::Expr::LVal(lval) => {
-            let element = id_table.get(lval).unwrap();
-            match element {
+            let element = id_table.get(lval);
+            if element.0.is_none() {
+                panic!("Unable to Find Value")
+            }
+            match element.0.unwrap() {
                 IdElement::Const(val) => (String::new(), *val),
                 IdElement::Var(_) => {
                     let mut counter = COUNTER.lock().unwrap();
                     *counter += 1;
-                    let out = format!("%{} = load @{}\n", *counter, lval);
+                    let out = format!("%{} = load @{}\n", *counter, element.1);
                     (out, *counter)
                 }
             }
